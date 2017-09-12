9a391b24631be5cd3e86f8b29a40078f09e0e67c
Artem Pavlov <javase@inbox.ru>
2017-09-12 08:14:50 +0300

    2. Сортировка User с использованием Comparator

--- lesson03/task03_03/src/main/java/apavlov/SortUser.java
+++ lesson03/task03_03/src/main/java/apavlov/SortUser.java
@@ -1,5 +1,8 @@
 package apavlov;
 
+
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
@@ -19,5 +22,38 @@ public class SortUser {
      */
     public Set<User> sort(List<User> list) {
         return new TreeSet<>(list);
+    }
+
+    /**
+     * The method sort list by length name.
+     *
+     * @param list - list users;
+     * @return - sort list by length name;
+     */
+    public List<User> sortNameLength(List<User> list) {
+        Collections.sort(list, new Comparator<User>() {
+            @Override
+            public int compare(User o1, User o2) {
+                return o1.getName().length() - o2.getName().length();
+            }
+        });
+        return list;
+    }
+
+    /**
+     * The method sort list by name or if name equals, sort by age.
+     *
+     * @param list - list users;
+     * @return - sort list by name or if names equals, sort by age;
+     */
+    public List<User> sortByAllFields(List<User> list) {
+        Collections.sort(list, new Comparator<User>() {
+            @Override
+            public int compare(User o1, User o2) {
+                int name = o1.getName().compareTo(o2.getName());
+                return name == 0 ? o1.getAge() - o2.getAge() : name;
+            }
+        });
+        return list;
     }
 }
--- lesson03/task03_03/src/test/java/apavlov/SortUserTest.java
+++ lesson03/task03_03/src/test/java/apavlov/SortUserTest.java
@@ -25,4 +25,30 @@ public class SortUserTest {
         String result = "[[name - 2; age - 15], [name - 1; age - 20], [name - 3; age - 25]]";
         assertThat(result, is(sortUser.sort(list).toString()));
     }
+
+    /**
+     * The test method for check sort users by length name.
+     *
+     * @throws Exception - check any errors;
+     */
+    @Test
+    public void whenSortByLengthName() throws Exception {
+        SortUser sortUser = new SortUser();
+        List<User> list = Arrays.asList(new User[]{new User("Alexander", 20), new User("Ivan", 15), new User("Sergey", 25)});
+        String result = "[[name - Ivan; age - 15], [name - Sergey; age - 25], [name - Alexander; age - 20]]";
+        assertThat(result, is(sortUser.sortNameLength(list).toString()));
+    }
+
+    /**
+     * The test method for check sort users by name or age.
+     *
+     * @throws Exception - check any errors;
+     */
+    @Test
+    public void whenSortByNameOrAge() throws Exception {
+        SortUser sortUser = new SortUser();
+        List<User> list = Arrays.asList(new User[]{new User("Ivan", 15), new User("Ivan", 10), new User("Alexander", 20)});
+        String result = "[[name - Alexander; age - 20], [name - Ivan; age - 10], [name - Ivan; age - 15]]";
+        assertThat(result, is(sortUser.sortByAllFields(list).toString()));
+    }
 }

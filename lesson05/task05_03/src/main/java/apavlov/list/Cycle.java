37bb28fc3cbd993d4861d6d93b59e0d2a0d9f276
Artem Pavlov <javase@inbox.ru>
2017-09-27 13:21:23 +0300

    5.3.4 Задан связанный список. Определить цикличность

--- lesson05/task05_03/src/main/java/apavlov/list/Cycle.java
+++ lesson05/task05_03/src/main/java/apavlov/list/Cycle.java
@@ -1,4 +1,80 @@
 package apavlov.list;
 
-public class Cycle {
+/**
+ * The class Cycle - check linked list on the vicious circle.
+ *
+ * @param <E> This describes my type parameter
+ * @author Pavlov Artem
+ * @since 27.09.2017
+ */
+public class Cycle<E> {
+    /**
+     * The method create new linked list is cycle or is not cycle.
+     *
+     * @param size       - size linked list;
+     * @param indexCycle - index for cycle; if index < 0 or index >= size - list is not cycle;
+     * @param fillValues - value for fill list;
+     * @return - link first element;
+     */
+    public Node<E> createLinkList(int size, int indexCycle, E fillValues) {
+        Node<E> first = size > 0 ? new Node<>(fillValues) : null;
+        if (first != null) {
+            Node<E> last = first;
+            Node<E> temp = indexCycle == 0 ? first : null;
+            for (int i = 1; i < size; i++) {
+                last = last.next = new Node<>(fillValues);
+                if (i == indexCycle) {
+                    temp = last;
+                }
+            }
+            if (temp != null) {
+                last.next = temp;
+            }
+        }
+        return first;
+    }
+
+    /**
+     * The method check is cycle list or is not cycle.
+     *
+     * @param first - link first element list`s;
+     * @return - true - is cycle; false - is not cycle;
+     */
+    public boolean hasCycle(Node<E> first) {
+        Node<E> x = null;
+        Node<E> y = null;
+        if (first != null) {
+            for (x = first, y = first.next; x != null && y != null && y.next != null && !x.equals(y); x = x.next, y = y.next.next) {
+            }
+        }
+        return x != null && y != null && x.equals(y);
+    }
+
+    /**
+     * The inner class Node - use for save link to next element type Node and value.
+     *
+     * @param <E> This describes my type parameter
+     * @author Pavlov Artem
+     * @since 27.09.2017
+     */
+    private class Node<E> {
+        /**
+         * The var - link on the next element.
+         */
+        private Node<E> next;
+
+        /**
+         * The var - value.
+         */
+        private E value;
+
+        /**
+         * The constructor for inner class Node.
+         *
+         * @param value - value;
+         */
+        Node(E value) {
+            this.value = value;
+        }
+    }
 }
--- lesson05/task05_03/src/test/java/apavlov/CycleTest.java
+++ lesson05/task05_03/src/test/java/apavlov/CycleTest.java
@@ -1,5 +1,54 @@
-import static org.junit.Assert.*;
+package apavlov;
 
+import apavlov.list.Cycle;
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * The test class CycleTest for class`s Cycle use junit4.
+ *
+ * @author Pavlov Artem
+ * @since 27.09.2017
+ */
 public class CycleTest {
+    /**
+     * The test method check correct work if list is null.
+     *
+     * @throws Exception - check any errors;
+     */
+    @Test
+    public void whenLinkedListIsNull() throws Exception {
+        Cycle<Integer> list = new Cycle<>();
+        assertThat(list.hasCycle(null), is(false));
+    }
 
+    /**
+     * The test method check correct work, if list is cycle.
+     *
+     * @throws Exception - check any errors;
+     */
+    @Test
+    public void whenLinkedListIsCycle() throws Exception {
+        Cycle<Integer> list = new Cycle<>();
+        assertThat(list.hasCycle(list.createLinkList(10, 0, null)), is(true));
+        assertThat(list.hasCycle(list.createLinkList(1, 0, null)), is(true));
+        assertThat(list.hasCycle(list.createLinkList(10, 9, null)), is(true));
+        assertThat(list.hasCycle(list.createLinkList(10, 5, null)), is(true));
+    }
+
+    /**
+     * The test method check correct work, if list is not cycle.
+     *
+     * @throws Exception - check any errors;
+     */
+    @Test
+    public void whenLinkedListIsNotCycle() throws Exception {
+        Cycle<Integer> list = new Cycle<>();
+        assertThat(list.hasCycle(list.createLinkList(10, -1, null)), is(false));
+        assertThat(list.hasCycle(list.createLinkList(0, 0, null)), is(false));
+        assertThat(list.hasCycle(list.createLinkList(10, 10, null)), is(false));
+        assertThat(list.hasCycle(list.createLinkList(10, 15, null)), is(false));
+    }
 }
